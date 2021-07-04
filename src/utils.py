b6901328b3998ad78c6044d01c12e28687273aca
sanchuan.csc <sanchuan.csc@alibaba-inc.com>
2021-07-04 23:36:37 +0800

    init commit for basic utils

---
+++ src/utils.py
@@ -0,0 +1,30 @@
+from subprocess import Popen,PIPE
+from multiprocessing import  Process
+import os
+
+class Execmd(object):
+    def __init__(self,cmd : str):
+        self.cmd = cmd
+
+    def get(self,raiseError=False) -> str:
+        p = Popen(self.cmd, shell=True, stdout= PIPE, stderr = PIPE)
+        stdout, stderr = p.communicate(timeout=2)
+
+        if stderr and raiseError:
+            raise Exception(stderr.decode(encoding='utf-8'))
+        return stdout.decode(encoding='utf-8')
+
+    def fire(self):
+        os.system(self.cmd)
+
+    def asyncFire(self):
+        p = Process(target=self.fire)
+        p.start()
+        return p.pid
+
+
+if __name__ == '__main__':
+    Execmd("curl http://www.baidu.com 2>&1 >/dev/null").asyncFire()
+    print("output:" + Execmd("curl http:/dafadsxxxf.com").get())
+    print(Execmd("curl http:/dafadsxxxf.com").get(raiseError=True))
+
