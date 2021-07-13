d3f1847042cccc48d1ba752c532a514aede92586
sanchuan.csc <sanchuan.csc@alibaba-inc.com>
2021-07-13 19:20:15 +0800

    exclude inet6 keyword

--- src/ops.py
+++ src/ops.py
@@ -32,7 +32,7 @@ def getOsType():
     return osset
 
 def getDefaultIp():
-    cmd = "ip a s | grep inet | grep -v '127.0.0.1' | head -1 | awk '{print $2,$NF}'"
+    cmd = "ip a s | grep inet |grep -v inet6| grep -v '127.0.0.1' | head -1 | awk '{print $2,$NF}'"
     ipinfo =  Execmd(cmd).get(raiseError=True)
     if ipinfo.strip():
         return ipinfo.split(" ")
